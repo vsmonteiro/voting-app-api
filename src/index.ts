76ee6e2a0f97a9bdc3242a7ed7546b370a4985de
Vinicius Monteiro <vmonteiro@gvdasa.com.br>
2021-11-21 22:45:37 -0300

    Add error handler with yup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { json } from "express";
+import "express-async-errors";
 import cors from "cors";
 import router from "./routes";
+import errorHandler from "./errorHandler";
 
 const app = express();
 
 app.use(cors());
 app.use(json());
 app.use(router);
+app.use(errorHandler);
 
 app.listen(3001);
